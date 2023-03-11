62d411dfbd01798be1bc46df87c161f59eaad6cf
mariarua <maria.rua1@udea.edu.co>
2023-03-11 10:31:50 -0500

    Create function to write and append a file

--- index.js
+++ index.js
@@ -8,11 +8,51 @@ const path = require("path");
 const readFile = async () => {
   try {
     const filePath = path.resolve(`${__dirname}/archivo.txt`);
-    const data = await fs.promises.readFile(filePath, "ascii");
+    const data = await fs.promises.readFile(filePath, "utf-8");
     console.log(data);
   } catch (error) {
     console.log(error);
   }
 };
 
-readFile();
+const writeFile = async () => {
+  try {
+    const filePath = path.resolve(`${__dirname}/archivo.txt`);
+    const message = "Estoy agregando mensajes a mi archivo";
+    const data = await fs.promises.writeFile(filePath, message, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const appendFile = async () => {
+  try {
+    const filePath = path.resolve(`${__dirname}/archivo.txt`);
+    const message = "ahora aqui append a mi archivo";
+    const data = await fs.promises.appendFile(filePath, message, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const writeAppenteReadFile = async () => {
+  try {
+    await readFile();
+    await writeFile();
+    await readFile();
+    await appendFile();
+    await readFile();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+writeAppenteReadFile();
