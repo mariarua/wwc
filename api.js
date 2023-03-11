31220036b8dfe298bc3ba2eeb42319112ee1b02d
mariarua <maria.rua1@udea.edu.co>
2023-03-11 11:18:25 -0500

    Create fetch for rick and mory api

---
+++ api.js
@@ -0,0 +1,16 @@
+//const fetch = require("node-fetch");
+// Cuando se usa el fetch descargado sale error
+// Se decidio que se iba a usar nativo y funciona bien
+
+const fetchApi = async (url) => {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    data.results.forEach((d) => console.log(d.id, d.name, d.gender));
+    //console.log(data.results);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = fetchApi;
