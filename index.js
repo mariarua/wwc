d281dcf857d3504af0aca98bf0df9f4396684f81
mariarua <maria.rua1@udea.edu.co>
2023-03-11 10:09:37 -0500

    Read a file with node

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const isOdd = require("is-odd");
 
 console.log(isOdd(4));
+
+const fs = require("fs");
+const path = require("path");
+
+const readFile = async () => {
+  try {
+    const filePath = path.resolve(`${__dirname}/archivo.txt`);
+    const data = await fs.promises.readFile(filePath, "ascii");
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+readFile();
