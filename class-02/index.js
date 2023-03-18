2de6e2dc8754df9d56cd065b554ecd42d0986851
mariarua <maria.rua1@udea.edu.co>
2023-03-18 10:25:13 -0500

    Create my first server with node

--- class-02/index.js
+++ class-02/index.js
@@ -3,4 +3,10 @@ const http = require("http");
 const HOST = "localhost";
 const PORT = 8000;
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  res.end("Este es mi primer servidor en node");
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Servidor corriendo en HTTP://${HOST}:${PORT}`);
+});
