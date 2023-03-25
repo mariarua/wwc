f430bddd3ca5b09f96ee8fff00d2d27541a554f9
mariarua <maria.rua1@udea.edu.co>
2023-03-25 09:59:21 -0500

    Add post method and create error middlewares

--- class-03/index.js
+++ class-03/index.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const PORT = 3000;
 
+const app = express();
+app.use(express.json());
+
+const errorLogger = (err, req, res, next) => {
+  console.log(err);
+  next(err);
+};
+
+const errorHandler = (err, req, res, next) => {
+  res.status(400).json({
+    message: err.message,
+  });
+};
 const products = [
   {
     id: 1,
@@ -23,8 +36,6 @@ const products = [
   },
 ];
 
-const app = express();
-
 app.get("/", (req, res) => {
   res.send("Esta es mi primera app en express");
 });
@@ -34,10 +45,23 @@ app.get("/api/v1/products", (req, res) => {
 });
 
 app.get("/api/v1/products/:productId", (req, res) => {
-  const productId = req.params;
-  console.log(req.params);
-  res.json({});
+  const { productId } = req.params;
+  const productIdInt = parseInt(productId);
+  const product = products.find((product) => product.id == productIdInt);
+  if (!product) {
+    throw new Error("Producto no encontrado");
+  }
+  res.json(product);
 });
+
+app.post("/api/v1/products", (req, res) => {
+  const product = req.body;
+  products.push(product);
+  res.json(products);
+});
+
+app.use(errorLogger);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Escuchando en http://localhost:${PORT}`);
